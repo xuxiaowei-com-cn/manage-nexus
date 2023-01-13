42384cf5834180dd409cfd3f93b947f423b955a1
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-01-13 13:23:17 +0800

    :sparkles: string 转 base64

--- src/base64.rs
+++ src/base64.rs
@@ -1,4 +1,25 @@
 /**
+ * string 转 base64
+ * <p>
+ * 来自：https://magiclen.org/base64-stream/
+ */
+pub fn string_to_base64(data: String) -> String {
+    use std::io::{Cursor, Read};
+
+    use base64_stream::ToBase64Reader;
+
+    let data_vec = data.as_bytes().to_vec();
+
+    let mut reader = ToBase64Reader::new(Cursor::new(data_vec));
+
+    let mut result = String::new();
+
+    reader.read_to_string(&mut result).unwrap();
+
+    return result;
+}
+
+/**
  * str 转 base64
  * <p>
  * 来自：https://magiclen.org/base64-stream/
@@ -17,6 +38,15 @@ pub fn str_to_base64(data: &str) -> String {
     reader.read_to_string(&mut result).unwrap();
 
     return result;
+}
+
+
+#[test]
+fn string_to_base64_test() {
+    let tmp = "Hi there, this is a simple sentence used for testing this crate. I hope all cases are correct.";
+    let data = tmp.to_string();
+    let result = string_to_base64(data);
+    assert_eq!("SGkgdGhlcmUsIHRoaXMgaXMgYSBzaW1wbGUgc2VudGVuY2UgdXNlZCBmb3IgdGVzdGluZyB0aGlzIGNyYXRlLiBJIGhvcGUgYWxsIGNhc2VzIGFyZSBjb3JyZWN0Lg==", result);
 }
 
 #[test]
