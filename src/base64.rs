f741782b25f3896a7a7cfb4f4e4e26cf56719800
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-01-13 13:28:06 +0800

    :sparkles: string 计算 basic

--- src/base64.rs
+++ src/base64.rs
@@ -40,6 +40,14 @@ pub fn str_to_base64(data: &str) -> String {
     return result;
 }
 
+/**
+ * string 计算 basic
+ */
+pub fn string_to_basic(username: String, password: String) -> String {
+    let tmp = username + ":" + &*password;
+    let base64 = string_to_base64(tmp);
+    return "Basic ".to_string() + &base64;
+}
 
 #[test]
 fn string_to_base64_test() {
@@ -54,4 +62,15 @@ fn str_to_base64_test() {
     let data = "Hi there, this is a simple sentence used for testing this crate. I hope all cases are correct.";
     let result = str_to_base64(data);
     assert_eq!("SGkgdGhlcmUsIHRoaXMgaXMgYSBzaW1wbGUgc2VudGVuY2UgdXNlZCBmb3IgdGVzdGluZyB0aGlzIGNyYXRlLiBJIGhvcGUgYWxsIGNhc2VzIGFyZSBjb3JyZWN0Lg==", result);
+}
+
+#[test]
+fn string_to_basic_test() {
+    let username_str = "xuxiaowei";
+    let password_str = "123456";
+
+    let username = username_str.to_string();
+    let password = password_str.to_string();
+    let result = string_to_basic(username, password);
+    assert_eq!("Basic eHV4aWFvd2VpOjEyMzQ1Ng==", result);
 }
